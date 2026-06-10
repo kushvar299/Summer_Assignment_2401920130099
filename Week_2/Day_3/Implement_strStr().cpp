e91e3e1434193e0aaf78c33bbc3159e9f2785782
kushvar299 <128236699+kushvar299@users.noreply.github.com>
2026-06-10 13:09:12 +0530

    Add strStr() implementation with two approaches
    Implemented strStr() using both backtracking and string::find methods.

--- Week_2/Day_3/Implement_strStr().cpp
+++ Week_2/Day_3/Implement_strStr().cpp
@@ -1,3 +1,29 @@
+//by backtracking
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        
+        int j=0;
+        for(int i=0;i<haystack.size();i++){
+            if(haystack[i]==needle[j]){
+                j++;
+                if(j==needle.size()){
+                    return (i-needle.size()+1);
+                }
+            }  
+            else{
+                i=i-j;
+                j=0;
+            }
+        }
+        return -1;
+    }
+};
+
+//by implementing strStr()
+//by using string:::find
+
 class Solution {
 public:
     int strStr(string haystack, string needle) {
